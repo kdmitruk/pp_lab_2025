a8cb3bdb3f905fbda33560f844a80dcc9582a63f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-02 10:28:14 +0200

    z8

---
+++ lab_01/z8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    //float avg = (float)(a+b+c)/3;
+    float avg = (a+b+c) / 3.f;
+    cout << avg << endl;
+
+    return 0;
+}
