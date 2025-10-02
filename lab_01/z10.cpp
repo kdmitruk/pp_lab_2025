ebe338095dedb529e7473e465f15d91e1b3ea588
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-02 10:44:28 +0200

    z10

---
+++ lab_01/z10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int  n;
+    float an, sn;
+    cin >> an >> sn >> n;
+
+    float a1 =(2*sn)/n-an;
+    cout << a1 << endl;
+
+    return 0;
+}
