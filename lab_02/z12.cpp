5231a8f172f49fb2e213ff0d60803f2fc0652311
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-09 09:15:06 +0200

    z16

---
+++ lab_02/z12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a+=1;
+    b++;
+    cout << a*b - 1;
+    return 0;
+}
---
+++ lab_02/z2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int N=30;
+    cout << N << endl;
+    //N=20;
+    return 0;
+}
