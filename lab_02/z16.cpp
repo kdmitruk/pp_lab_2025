920a672f5f673cb77e4cb422ee294cc9ac29021b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-09 09:40:51 +0200

    z15

--- lab_02/z16.cpp
+++ lab_02/z16.cpp
@@ -6,6 +6,23 @@ int main()
 {
     char c;
     cin >> c;
-    cout << (c >= 'a' && c <= 'z');
+    bool is_lower = c >= 'a' && c <= 'z';
+
+
+    if (is_lower)
+       cout<<"true";
+    else
+       cout<<"false";
+    cout << endl;
+
+    cout << (is_lower ? "true":"false");
+    cout << endl;
+
+    cout << boolalpha<<is_lower;
+    cout << endl;
+
+
+
+
     return 0;
 }
