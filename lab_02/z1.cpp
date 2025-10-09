5284b2599e55de4848fcfc0bac1b2e9aebeee91a
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-09 08:48:02 +0200

    z1

--- lab_02/z1.cpp
+++ lab_02/z1.cpp
@@ -16,3 +16,22 @@ int main()
     std::cout << a << " " << b;
     return 0;
 }
+
+
+// int main() {
+//     double a;
+//     cout << sizeof(int8_t) << endl;
+//     cout << sizeof(int16_t) << endl;
+//     cout << sizeof(int32_t) << endl;
+//     cout << sizeof(int64_t) << endl;
+//     cout << endl;
+//     cout << sizeof(char) << endl;
+//     cout << sizeof(short) << endl;
+//     cout << sizeof(int) << endl;
+//     cout << sizeof(long) << endl;
+//     cout << sizeof(long long) << endl;
+//     cout << endl;
+//     cout << sizeof(float) << endl;
+//     cout << sizeof(double) << endl;
+//     return 0;
+// }
