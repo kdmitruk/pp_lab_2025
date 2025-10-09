f2884a0a2ef19aded3b8df8d77b7e8902c9d0068
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-09 09:58:16 +0200

    z7

---
+++ lab_02/z7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << sqrt(pow(a, 2)+pow(b, 2));
+    cout << endl;
+    cout << hypot(a, b);
+
+    return 0;
+}
