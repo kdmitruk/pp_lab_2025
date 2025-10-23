ed4c9f992c5c5594d293352166c0e782eb8e0a72
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-23 09:11:28 +0200

    z1

---
+++ lab_03/z1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+   /* int i = 0;
+   while (i<x) {
+        cout << i++<<endl;
+    }*/
+    for (int i=0;i<x;i++)
+        cout<<i<<endl;
+
+
+    return 0;
+}
+
+
+
