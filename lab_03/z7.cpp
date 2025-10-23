f7d8372281c3fae6cfe0696b438e4da79039c896
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-23 09:53:29 +0200

    z7

---
+++ lab_03/z7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    for(int i = 0;i<=10;i++){
+        for(int j = 0;j<=10;j++){
+            cout<<setw(5)<<j*i<<" ";
+        }
+        cout<<endl;
+    }
+}
