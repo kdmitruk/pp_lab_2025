972d32a488db0fcdf74725abe2b035ff95de1050
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-10-30 11:02:08 +0100

    z13

---
+++ lab_04/z13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    const int SIZE = 7;
+    int tab[7] = {64, 34, 25, 12, 22, 11, 90};
+
+    for (int i = 0; i < SIZE - 1; ++i) {
+        for (int j = 0; j < SIZE - i - 1; ++j) {
+            if (tab[j] > tab[j + 1]) {
+                std::swap(tab[j], tab[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << tab[i] << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
