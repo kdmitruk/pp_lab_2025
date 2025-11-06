e0c28fa34dc1059189548f088acf4a43079908e2
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-11-06 10:25:28 +0100

    zn4

---
+++ lab_04/zn4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    char t1[100]="abcd";
+    char t2[100]="abc";
+
+    //printf("%d",strcmp(t1,t2));
+    int result=0;
+    int i;
+    for (i=0; t1[i]!='\0'||t2[i]!='\0'; i++) {
+        if (t1[i]<t2[i]) {
+            result=-i;
+            break;
+        }
+        if (t1[i]>t2[i]) {
+            result=i+1;
+            break;
+        }
+    }
+    if (result!=0) {
+        printf("%d", result);
+    }
+    else {
+        if (t1[i]=='\0'&&t2[i]=='\0') {
+            printf("0");
+        }
+        else {
+            if (t1[i]=='\0') {
+                printf("-%d", i);
+            }
+            else {
+                printf("%d", i+1);
+            }
+        }
+    }
+
+
+
+
+    return 0;
+}
