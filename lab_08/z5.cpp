e17b872b71f08b8cf7b00218273589f2940f469f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-12-04 09:33:13 +0100

    z5

---
+++ lab_08/z5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int* b){
+    int tmp;
+    tmp = a;
+    a = *b;
+    *b = tmp;
+}
+
+int main() {
+    int a = 1;
+    int b = 2;
+
+    swap(a, &b);
+
+    cout << a <<" "<< b << endl;
+
+    return 0;
+}
