f1691aa7acefc3906a5fee96826e10813a0742f9
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-12-18 10:10:27 +0100

    z2 - staly rozmiar tablicy 2d

--- lab_10/z2.cpp
+++ lab_10/z2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
 
@@ -21,6 +22,31 @@ void populate_arr(unsigned char* arr, int rows, int cols){
     }
 }
 
+void convolution(unsigned char arr[5][10], int rows, int cols){
+    unsigned char temp_arr[5][10];
+    unsigned int sum, counter;
+
+    for(int y = 0; y < rows; y++){
+        for(int x = 0; x < cols; x++){
+            sum=0;
+            counter=0;
+            for(int ly=y-1; ly<=y+1; ly++){
+                if(ly>=0 && ly<rows){
+                    for(int lx=x-1; lx<=x+1; lx++){
+                        if(lx>=0 && lx<cols){
+                            sum += arr[ly][lx];
+                            counter++;
+                        }
+                    }
+                }
+            }
+            temp_arr[y][x]=round((float)sum/counter);
+        }
+    }
+
+    memcpy(arr, temp_arr, sizeof(unsigned char)*rows*cols);
+}
+
 int main(){
     const int ROWS = 5;
     const int COLS = 10;
@@ -28,6 +54,12 @@ int main(){
     unsigned char arr[ROWS][COLS];
     populate_arr((unsigned char*)arr, ROWS, COLS);
     show_arr((unsigned char*)arr, ROWS, COLS);
+
+    cout<<endl;
+
+    convolution(arr, ROWS, COLS);
+    show_arr((unsigned char*)arr, ROWS, COLS);
+
 
     return 0;
 }
