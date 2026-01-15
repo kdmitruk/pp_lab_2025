387e31436e69cea667eda61a22af760cacd1c160
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-01-15 10:09:27 +0100

    z7

---
+++ lab_11/z7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << std::atoi("12")+10;
+    // char arr[11]="1234567890";
+    // int value = std::atoi(arr);
+    // std::cout << value+10;
+    return 0;
+}
